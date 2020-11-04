1d85363a5c41f75c8a6c158a002018874ca52e3a
Barry750 <heliogeno09@hotmail.com>
2020-11-04 17:14:36 -0600

    Repository updated

--- routes/user1.js
+++ routes/user1.js
@@ -1,13 +1,14 @@
 const express = require ('express');
 const user1 = express.Router();
 const db = require('../config/database');
+const jwt = require('jsonwebtoken');
 
 user1.post("/", async (req, res, next)=>{
     const { user_name, user_mail, user_password} = req.body;
 
     if (user_name && user_mail && user_password){
     let query = "INSERT INTO user1 (user_name, user_mail, user_password)";
-    query += `VALUES ('${user_name}', '${user_mail}', '${user_password}')`;
+    query += `VALUES ('${user_name}', '${user_mail}', '${user_password}');`;
 
     const rows = await db.query(query);
 
@@ -17,6 +18,32 @@ user1.post("/", async (req, res, next)=>{
     return res.status(500).json({code: 500, message: "Ocurrió un error."});
 }
 return res.status(500).json({code: 500, message: "Ocurrió un error."});
+});
+
+user1.post("/login", async (req, res, next) =>
+{
+    //esta parte sirve para pedir el usuario y contraseña de la persona
+    const {user_mail, user_password} = req.body;
+    const query = `SELECT * FROM user1 WHERE user_mail = '${user_mail}' AND user_password = '${user_password}';`; /*esta es la sintaxis para que se traiga el usuario y contraseña*/
+    const rows = await db.query(query);
+    
+    if (user_mail && user_password)
+    {
+        if (rows.length == 1)
+        {
+            const token = jwt.sign(
+            {
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail
+            }, "debugkey");
+            return res.status(200).json({code: 200, message: token});
+        }
+        else
+        {
+            return res.status(401).json({code: 401, message: "Usuario y/o contraseña incorrectos"});
+        }
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"});
 });
 
 user1.get ("/", async(req, res, next) =>
